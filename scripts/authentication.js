b53454b42580240efd2a27f1c19bc272f6a5d5d1
Michelle Ao <michelle388q@gmail.com>
2022-11-01 11:45:35 -0700

    demo09

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -14,16 +14,16 @@ var uiConfig = {
             // Before this works, you must enable "Firestore" from the firebase console.
             // The Firestore rules must allow the user to write. 
             //------------------------------------------------------------------------------------------
-            var user = authResult.user;                            // get the user object from the Firebase authentication database
-            if (authResult.additionalUserInfo.isNewUser) {         //if new user
-                db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-                        name: user.displayName,                    //"users" collection
-                        email: user.email,                         //with authenticated user's ID (user.uid)
-                        country: "Canada",                      //optional default profile info      
-                                                        school: "BCIT"                          //optional default profile info
+            var user = authResult.user; // get the user object from the Firebase authentication database
+            if (authResult.additionalUserInfo.isNewUser) { //if new user
+                db.collection("users").doc(user.uid).set({ //write to firestore. We are using the UID for the ID in users collection
+                        name: user.displayName, //"users" collection
+                        email: user.email, //with authenticated user's ID (user.uid)
+                        country: "Canada", //optional default profile info      
+                        school: "BCIT" //optional default profile info
                     }).then(function () {
                         console.log("New user added to firestore");
-                        window.location.assign("main.html");       //re-direct to main.html after signup
+                        window.location.assign("main.html"); //re-direct to main.html after signup
                     })
                     .catch(function (error) {
                         console.log("Error adding new user: " + error);
